a0e1d8014ac83b1ca97c6f9d1973c58bf56ba5c1
MsNiyaJ <shaniyasmalcolm@gmail.com>
2022-06-16 21:48:46 -0400

    created new task tracker app with header

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+<script lang="ts">
+export default {
+  name: 'Button',
+  props: {
+    text: {
+      type: String,
+      default: 'Button',
+    },
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('clicked');
+    },
+  },
+  onClick() {},
+};
+</script>
+<style></style>
